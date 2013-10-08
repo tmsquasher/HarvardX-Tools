56ef531239fc2f36aef5aba56c1b7f9b5bf3fefd
Tommy Mullaney <tmullaney@college.harvard.edu>
2013-10-08 19:26:18 -0400

    clearer docstrings

--- src/main/python/personClickUtils.py
+++ src/main/python/personClickUtils.py
@@ -4,7 +4,7 @@ Handy derived data and analysis functions for working with person-click
 datasets in Pandas. Most of these functions get passed a person-click 
 DataFrame.
 
-This product includes GeoLite data created by MaxMind, available from
+This script uses GeoLite data created by MaxMind, available from
 http://www.maxmind.com.
 
 Created on September 18, 2013 
@@ -20,12 +20,13 @@ import datetime
 import dateutil.parser
 import trackingLogParser
 
-'''
-DERIVED DATA
-Add columns to person-click datasets. Be careful running on very large 
-DataFrames, as these functions can be slow.
-'''
+# DERIVED DATA
+# Add columns to person-click datasets. Be careful running on very large 
+# DataFrames, as these functions can be slow.
 def getAgentInfo(df):
+    """
+    Returns a dataframe with device, operating system, and browser data.
+    """
     from ua_parser import user_agent_parser
     # user_agent_parser
     #   module: https://github.com/tobie/ua-parser
@@ -41,6 +42,9 @@ def getAgentInfo(df):
     return df['agent'].apply(getInfo)
 
 def getIPInfo(df, geolitecity_dat):
+    """
+    Returns a dataframe with local time and geolocation data.
+    """
     import pygeoip
     # pygeoip
     #   module: https://pypi.python.org/pypi/pygeoip/
@@ -71,23 +75,27 @@ def getIPInfo(df, geolitecity_dat):
 
     return df[['time', 'ip']].apply(getInfo, axis=1)
 
-'''
-DATA INTEGRITY
-Verify completeness/accuracy of a person-click dataset.
-'''
+
+# DATA INTEGRITY
+# Verify completeness/accuracy of a person-click dataset.
 def loadPersonClick(personclick_csv, cols=None):
-    # Full log person-click datasets can be too large for Pandas'
-    # built-in CSV reader to work properly. This function loads a 
-    # dataframe in chunks and concatenates instead. You can optionally
-    # pass a list of column names to limit which columns get read in
-    # and save memory.
+    """
+    Full log person-click datasets can be too large for Pandas'
+    built-in CSV reader to work properly. This function loads a 
+    dataframe in chunks and concatenates instead. You can optionally
+    pass a list of column names to limit which columns get read in
+    and save memory.
+    """
     it = pd.read_csv(personclick_csv, iterator=True, chunksize=1000, usecols=cols)
     return pd.concat([chunk for chunk in it], ignore_index=True)
 
 def getUniqueDiscardPatterns(df_discards):
-    # Because we discard log items in parsing a person-click dataset,
-    # we need to justify each pattern we're discarding, and manually
-    # check the noise.
+    """
+    Because we discard log items in parsing a person-click dataset,
+    we need to justify each pattern we're discarding, and manually
+    check the noise. This function returns a dataframe of these
+    patterns and the number of occurrences of each.
+    """
     def replaceHashes(event_type):
         hashless = re.sub(r'[0-9a-f]{24,32}', '{HASH}', event_type)
         return re.sub(r'(lecture_[0-9]+)|([a-z]+_l[0-9]+(_[a-z]+)+)', '{NONHASH_ID}', hashless) # vert|cond|poll
@@ -95,32 +103,39 @@ def getUniqueDiscardPatterns(df_discards):
     return df_discards.event_type.apply(replaceHashes).value_counts()
 
 def countAnonymousUserEvents(df, pct=True):
-    # When users aren't logged in, logged events don't have usernames.
-    # When we don't have usernames, it's harder to do person-level
-    # analyses.
+    """
+    When users aren't logged in, logged events don't have usernames.
+    This function returns the number of events without usernames.
+    """
     n_anonymous = len(df[df.actor.isnull()])
     n_total = len(df)
     return float(n_anonymous) / n_total if pct else n_anonymous
 
 def getAxisLookupFails(df):
-    # Sometimes courseware events will be logged that can't be found
-    # in the Course Axes. When this happens, we don't have a meaningful
-    # 'object_name' in the person-click dataset.
+    """
+    Sometimes courseware events will be logged that can't be found
+    in the Course Axes. When this happens, we don't have a meaningful
+    'object_name' in the person-click dataset. This function returns
+    a dataframe of all lookup fails.
+    """
     return df[df.object_name.apply(lambda x: 'Axis Lookup Failed' in str(x))]
 
 def makeVerbExampleTable(df):
-    # Makes a DataFrame where each type of verb in the dataset gets
-    # a row, and examples are filled in for the object, result, meta,
-    # event_type, and event columns.
+    """
+    Makes a DataFrame where each type of verb in the dataset gets
+    a row, and examples are filled in for the object, result, meta,
+    event_type, and event columns.
+    """
     ex_cols = ['verb', 'object_name', 'object_type', 'result', 'meta', 'event', 'event_type']
     return df[ex_cols].groupby(df.verb).last()
 
 def makePersonLevel(df):
-    # Makes a DataFrame where rows are unique usernames, columns are
-    # verb types, and values are the number of occurrences of each 
-    # verb type per user. Also includes a column for 'days_active'.
-    # Note: 'df' only needs to have columns for actor, verb, and time.
-
+    """
+    Makes a DataFrame where rows are unique usernames, columns are
+    verb types, and values are the number of occurrences of each 
+    verb type per user. Also includes a column for 'days_active'.
+    NOTE: 'df' only needs to have columns for actor, verb, and time.
+    """
     # calc days active (rough measure based on UTC)
     user_dates = df[["actor", "time"]]
     user_dates["time"] = pd.to_datetime(user_dates["time"]).apply(lambda x: x.date())
@@ -130,7 +145,7 @@ def makePersonLevel(df):
     user_verbs = df[["actor", "verb"]].groupby([df["actor"], df["verb"]]).size().reset_index()
     user_verbs = user_verbs.pivot(index="actor", columns="verb", values=0)
     
-    result = pd.DataFrame(user_verbs, columns=possible_verbs).fillna(0)
+    result = pd.DataFrame(user_verbs, columns=trackingLogParser.possible_verbs).fillna(0)
     result["days_active"] = user_dates
     
     return result
